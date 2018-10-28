49b9aa4a726468d14aec78cb405c68b4eb79db62
alegu <alegu@Asus-Alex.homenet.telecomitalia.it>
2018-10-28 19:43:46 +0100

    Versione internazionale

--- src/provagit/ProvaGit.java
+++ src/provagit/ProvaGit.java
@@ -15,7 +15,7 @@ public class ProvaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ciao a tutti");
+        System.out.println("Welcome to Git");
     }
     
 }
